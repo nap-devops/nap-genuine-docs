a9b3753cd1af05a07598d12205be6d54e34dcfde
tanatip <>
2023-04-12 16:24:21 +0700

    Update server index.js

--- server/index.js
+++ server/index.js
@@ -215,7 +215,8 @@ app.get('/message', (req, res) => {
 
 app.listen(3030, () => {
     console.log('server start on port 3030');
-    const allFiles = getAllFiles(path.join(__dirname, '../data'));
+    // const allFiles = getAllFiles(path.join(__dirname, '../data'));
+    const allFiles = getAllFiles('../napbiotec');
     console.log("########## List all COA ##########");
     console.log(allFiles);
 });
